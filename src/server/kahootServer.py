a956d40aded9e1335bf6d7802de792b6f07be8c0
Tobiloba Kujore <91623811+Toblobs@users.noreply.github.com>
2021-11-17 08:16:25 +0000

    v1.0.5
    - Moved question orientation to kModules/kQuestions.py
    - Hashed out #signal for development later

--- src/server/kahootServer.py
+++ src/server/kahootServer.py
@@ -1,91 +1,13 @@
 ### Hosted on Github at @Toblobs
 ### A Synergy Studios Project
 
-version = '1.0.4'
+version = '1.0.5'
 
 from netlib import Server, Signal
 from netlib.net.server.connection import Connection
 from uuid import uuid4
 
-
-#==============================================================#
-
-
-class Question:
-    """A singular representation of a question
-       in the program. Can edit itself and is passed to
-       json in the save."""
-
-    def __init__(self, question, correct):
-        self.question = question
-        self.correct = correct
-
-        self.uuid = uuid4()
-
-    def check_correct(self, resp):
-
-        pass
-
-        ### Example of (abstract?)
-    
-
-class SimpleAnswerQuestion(Question):
-    """A question that has a simple question and answer.
-       Inherits from class <Question>"""
-
-    def __init__(self, question, correct):
-
-        super().__init__(question, correct)
-
-    def check_correct(self, resp):
-
-        super().check_correct(resp)
-
-class MultipleAnswerQuestion(Question):
-    """A question that is multiple-answerable.
-       Between 2 to 4 choices are allowed. Only one is correct.
-       Inherits from class <Question>"""
-
-    def __init__(self, question, correct, answers):
-
-        super().__init__(question, correct)
-        
-        self.answer1 = answers[0]
-        self.answer2 = answers[1]
-
-        if len(answers) >= 3:
-            self.answer3 = answers[2]
-            
-        if len(answers) >= 4:
-            self.answer4 = answers[3]
-
-    def check_correct(self, resp):
-
-        super().check_correct(resp)
-
-        
-    
-class TrueOrFalseQuestion(Question):
-    """This question is always true or false.
-       Inherits from class <Question>"""
-
-    def __init__(self, question, correct, answers):
-
-        super().__init__(question, correct)
-
-    def check_correct(self, resp):
-
-        super().check_correct(resp)
-
-
-class QuestionMaker:
-    """Class which makes questions. It then returns all the questions made
-       in that session, and resets itself."""
-
-    pass
-
-#==============================================================#
-
+from kModules.kQuestions import *
 
 class KahootServer:
     """The server, which creates Question objects and
@@ -111,18 +33,16 @@ class KahootServer:
 
     def stop(self):
         self._server.exit()
-
-
-correct = 'B'
-
-with KahootServer() as ks:
-    
-    @ks._server.OnConnection
-    def handle_connection(conn):
         
-        @conn.OnSignal("is_answer_correct")
-        def is_correct(signal):
-            correct = (signal.payload.answer == correct_answer): #Make correct
+#correct = 'B'
 
-             conn.send(Signal({"correct": correct, "id": question_id}, "answer_status")) 
+#with KahootServer() as ks:
+    
+    #@ks._server.OnConnection
+    #def handle_connection(conn):
+        
+        #@conn.OnSignal("is_answer_correct")
+        #def is_correct(signal):
+            #correct = (signal.payload.answer == correct_answer) #Make correct
 
+            #conn.send(Signal({"correct": correct, "id": question_id}, "answer_status")) 
