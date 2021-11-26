153e483cff928f4cb4e47f24d966f880ee1ca2cc
Tobiloba Kujore <91623811+Toblobs@users.noreply.github.com>
2021-11-26 12:53:29 +0000

    v.1.0.3
    - Added version tag
    - Added connection via #signal to server
    
    - Added UUID
    
    - Moved everything to Client class

--- src/client/KahootClient.py
+++ src/client/KahootClient.py
@@ -1,14 +1,62 @@
 ### Hosted on Github at @Toblobs
 ### A Synergy Studios Project
 
+version = '1.0.3'
 
-from netlib import Server, Client, Signal
+from netlib import Client, Signal
 from netlib.net.server.connection import Connection
+from uuid import uuid4
 
-c = Client()
 
-@c.OnSignal("answer_status")
+class KahootClient:
+    """The client, which accepts questions and
+       answers them by sending back socket data."""
 
-def answer_status(signal):
-    print(f"isCorrect: {signal.payload.correct}")
+
+    def __init__(self):
+
+        self.ip = "10.130.65.253"
+        self.port = 4747
+
+        self.loc = 'EU-1'
+
+        self._client = Client(self.ip, self.port)
+
+        self.uuid4 = uuid4()
+
+    def setup_client(self, c: Client):
+
+        @self._client.OnConnect
+        def handle_connnection():
+
+             print(f"[+] Connected to server {self.loc} at {self.ip}:{self.port}")
+             self.send(Signal({"uuid": self.uuid4}, "/uuid/set"))
+
+             print(f"[+] Sent server {self.loc} UUID: <{self.uuid4}>")
+    
+             @self._client.OnSignal("/message")
+             def display_message(signal: Signal):
+                
+                content = signal.payload.content
+                print(content)
+
+    def connect(self):
+        
+        self.setup_client(self._client)
+        self._client.connect()
+
+    def send(self, data):
+        
+        self._client.send(data)
+
+
+
+
+#-------------------------------------------------------------------#
+
+kClient = KahootClient()
+
+kClient.connect()
+
+
 
